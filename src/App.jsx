86395baea6d3fac420d0c69b4e47e59c35da0900
savarniknandinee <80510308+savarniknandinee@users.noreply.github.com>
2024-10-20 23:29:44 +0530

    wohooo

--- src/App.jsx
+++ src/App.jsx
@@ -15,45 +15,43 @@ import SemesterTT from './components/SemesterTT';
 import AddSubject from './components/AddSubject';
 import CreateTimetable from './components/CreateTimetable';
 import AuthPage from './components/AuthPage';
+import { AuthProvider } from './components/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <Router>
-      <Background>
-        <div className="App">
-          <Banner />
-          <Routes>
-            <Route path="/" element={
-              <>
-                <Carousel />
-                <Section />
-                <HowToUse />
-                <Connect />
-              </>
-            } />
-            <Route path="/auth" element={<AuthPage />} />
-            <Route path="/add-teacher" element={<AddTeacher />} />
-            <Route path="/add-room" element={<AddRoom />} />
-            <Route path="/add-lab" element={<AddLab />} />
-            {/* {[1, 2, 3, 4, 5, 6, 7, 8].map(semester => (
-              <Route
-                key={semester}
-                path={`/semester/${semester}`}
-                element={<SemesterPage />}
-              />
-            ))} */}
-            <Route
-              path="/semester/:semester"
-              element={<SemesterPage />}
-            />
-            <Route path="/semester/:semester/timetable/:section" element={<SemesterTT />} />
-            <Route path="/semester/:semester/add-subject" element={<AddSubject />} />
-            <Route path="/semester/:semester/create-timetable" element={<CreateTimetable />} />
-          </Routes>
-          <Footer />
-        </div>
-      </Background>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Background>
+          <div className="App">
+            <Banner />
+            <Routes>
+              <Route path="/" element={
+                <>
+                  <Carousel />
+                  <Section />
+                  <HowToUse />
+                  <Connect />
+                </>
+              } />
+              <Route path="/auth" element={<AuthPage />} />
+              <Route path="/add-teacher" element={<AddTeacher />} />
+              <Route path="/add-room" element={<AddRoom />} />
+              <Route path="/add-lab" element={<AddLab />} />
+              <Route path="/semester/:semester" element={<SemesterPage />} />
+              <Route path="/semester/:semester/timetable/:section" element={<SemesterTT />} />
+              <Route path="/semester/:semester/add-subject" element={<AddSubject />} />
+              <Route path="/semester/:semester/create-timetable" element={
+                <ProtectedRoute>
+                  <CreateTimetable />
+                </ProtectedRoute>
+              } />
+            </Routes>
+            <Footer />
+          </div>
+        </Background>
+      </Router>
+    </AuthProvider>
   );
 }
 
